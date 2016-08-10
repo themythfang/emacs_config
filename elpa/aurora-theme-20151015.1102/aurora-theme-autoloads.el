e3ec01fdde2d9df62b9c0c8a312f3bda9f178996
yuanfang <yuanfang@jidemail.com>
2016-08-10 10:25:22 +0800

    bookmark binding

---
+++ elpa/aurora-theme-20151015.1102/aurora-theme-autoloads.el
@@ -0,0 +1,24 @@
+;;; aurora-theme-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+(add-to-list 'load-path (or (file-name-directory #$) (car load-path)))
+
+;;;### (autoloads nil "aurora-theme" "aurora-theme.el" (22421 60669
+;;;;;;  400126 827000))
+;;; Generated autoloads from aurora-theme.el
+
+(and load-file-name (boundp 'custom-theme-load-path) (add-to-list 'custom-theme-load-path (file-name-as-directory (file-name-directory load-file-name))))
+
+;;;***
+
+;;;### (autoloads nil nil ("aurora-theme-pkg.el") (22421 60669 444779
+;;;;;;  967000))
+
+;;;***
+
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; End:
+;;; aurora-theme-autoloads.el ends here
--- modules/yf-helm.el
+++ modules/yf-helm.el
@@ -1,8 +1,8 @@
 (defun yf-helm-setting ()
-  (global-set-key (kbd "C-q b") 'helm-bookmark)
-  (global-set-key (kbd "C-q r") 'helm-bookmark-rename)
-  (global-set-key (kdb "C-q d") 'helm-bookmark-run-delete))
-
+  (global-set-key (kbd "C-q") nil)
+  (global-set-key (kbd "C-q C-q") 'helm-bookmarks)
+  (global-set-key (kbd "C-q C-r") 'helm-bookmark-rename)
+  (global-set-key (kbd "C-q C-d") 'bookmark-delete))
 (setq yf-helm-hook' yf-helm-setting)
 
 (provide 'yf-helm)
