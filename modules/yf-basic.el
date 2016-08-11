0904f9077162a361d189c8a41a74349682f2d2f6
yuanfang <yuanfang@jidemail.com>
2016-08-11 20:43:01 +0800

    show line number

--- modules/yf-basic.el
+++ modules/yf-basic.el
@@ -1,6 +1,7 @@
 (defun yf-basic-setting ()
   (global-set-key (kbd "C-c C-g") 'grep-find)
-  (read-only-mode t))
+  (read-only-mode t)
+  (linum-mode t))
 
 (setq yf-basic-hook 'yf-basic-setting)
 (provide'yf-basic)
