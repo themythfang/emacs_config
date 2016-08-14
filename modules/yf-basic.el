7b95b9c1b39a48337ab7ce4d48e6265169bbc7d4
yuanfang <yuanfang_cs@163.com>
2016-08-14 15:37:58 +0800

    set yf-basic to minor mode prelude-mode. add some new key bindings for previoud/next buffer support for grep multiple buffer.

--- modules/yf-basic.el
+++ modules/yf-basic.el
@@ -2,11 +2,16 @@
   (global-set-key (kbd "C-c C-g") 'grep-find)
   (global-set-key (kbd "C-x C-i") 'previous-buffer)
   (global-set-key (kbd "C-x C-o") 'next-buffer)
-  (read-only-mode t)
   (linum-mode t))
 (setq yf-basic-hook 'yf-basic-setting)
 
+(defun yf-basic-text-setting()
+  (read-only-mode t))
+(setq yf-basic-text-hook 'yf-basic-text-setting)
+
 (add-hook 'prelude-mode-hook (lambda ()
                                 (run-hooks 'yf-basic-hook)))
+(add-hook 'prelude-c-mode-common-hook (lambda ()
+                                (run-hooks 'yf-basic-text-hook)))
 
 (provide'yf-basic)
--- modules/yf-theme.el
+++ modules/yf-theme.el
@@ -1,5 +1,9 @@
 (require 'prelude-lisp)
+
 (prelude-require-packages '(calmer-forest-theme))
+(prelude-require-packages '(grep-a-lot))
 
 (load-theme 'calmer-forest t)
+(require 'grep-a-lot)
+
 (provide 'yf-theme)
