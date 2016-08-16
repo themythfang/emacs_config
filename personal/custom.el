aafabd4b7f11f3519abd234284f6f867a62b1b23
yuanfang <yuanfang_cs@163.com>
2016-08-17 01:57:11 +0800

    use rgrep instead of grep

--- personal/custom.el
+++ personal/custom.el
@@ -3,9 +3,17 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(grep-files-aliases
+   (quote
+    (("all" . "* .*")
+     ("cc" . "*.cc *.cxx *.cpp *.C *.CC *.c++ *.java"))))
  '(grep-find-command
    (quote
-    ("find . -type f -exec grep --include=\"*.cpp\" --include=\"*.h\" --include=\"*.java\" --color -nH -e \"\" {} +" . 96))))
+    ("find . -type f -exec grep -I --color=always --exclude=\"*.img\" -nH -e \"\" {} +" . 71)))
+ '(grep-find-ignored-files
+   (quote
+    ("*.img" "*.md" ".classpath" "*.img" ".#*" "*.test" "*.o" "*~" "*.bin" "*.lbin" "*.so" "*.a" "*.ln" "*.blg" "*.bbl" "*.elc" "*.lof" "*.glo" "*.idx" "*.lot" "*.fmt" "*.tfm" "*.class" "*.fas" "*.lib" "*.mem" "*.x86f" "*.sparcf" "*.dfsl" "*.pfsl" "*.d64fsl" "*.p64fsl" "*.lx64fsl" "*.lx32fsl" "*.dx64fsl" "*.dx32fsl" "*.fx64fsl" "*.fx32fsl" "*.sx64fsl" "*.sx32fsl" "*.wx64fsl" "*.wx32fsl" "*.fasl" "*.ufsl" "*.fsl" "*.dxl" "*.lo" "*.la" "*.gmo" "*.mo" "*.toc" "*.aux" "*.cp" "*.fn" "*.ky" "*.pg" "*.tp" "*.vr" "*.cps" "*.fns" "*.kys" "*.pgs" "*.tps" "*.vrs" "*.pyc" "*.pyo")))
+ '(grep-highlight-matches (quote always)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
