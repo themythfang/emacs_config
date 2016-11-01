87469f3f01fb15270681fd783fa7e0f3a60efc46
yuanfang <yuanfang@jidemail.com>
2016-11-01 10:51:35 +0800

    Set offset from 2 to 4 Set default encoding to UTF-8 Set c-code-style lineup to specification style.

--- modules/yf-basic.el
+++ modules/yf-basic.el
@@ -1,8 +1,31 @@
+;; Basic encoding
+(setq utf-translate-cjk-mode nil) ; disable CJK coding/encoding (Chinese/Japanese/Korean characters)
+(set-language-environment 'utf-8)
+(set-keyboard-coding-system 'utf-8-mac) ; For old Carbon emacs on OS X only
+(setq locale-coding-system 'utf-8)
+(set-default-coding-systems 'utf-8)
+(set-terminal-coding-system 'utf-8)
+(unless (eq system-type 'windows-nt)
+  (set-selection-coding-system 'utf-8))
+(prefer-coding-system 'utf-8)
+
+(defun c-lineup-streamop (langelem)
+  ;; lineup stream operators
+  (save-excursion
+    (let* ((relpos (cdr langelem))
+           (curcol (progn (goto-char relpos)
+                          (current-column))))
+      (re-search-forward "<<\\|>>" (c-point 'eol) 'move)
+      (goto-char (match-beginning 0))
+      (- (current-column) curcol))))
+
 (defun yf-basic-setting ()
   (global-set-key (kbd "C-c C-g") 'rgrep)
   (global-set-key (kbd "C-x C-i") 'previous-buffer)
   (global-set-key (kbd "C-x C-o") 'next-buffer)
   (global-set-key (kbd "C-x C-h") 'recentf-open-files)
+  (c-set-offset 'arglist-intro '+)
+  (c-set-offset 'stream-op 'c-lineup-streamop)
   (linum-mode t))
 (setq yf-basic-hook 'yf-basic-setting)
 
