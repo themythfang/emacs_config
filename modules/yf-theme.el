56e77346c41d21bc33598e7240b61608ebd0e673
yuanfang <yuanfang@jidemail.com>
2016-11-03 12:03:43 +0800

    User autumn-light theme

--- modules/yf-theme.el
+++ modules/yf-theme.el
@@ -1,9 +1,17 @@
 (require 'prelude-lisp)
 
-(prelude-require-packages '(calmer-forest-theme))
-(prelude-require-packages '(grep-a-lot))
+;; (load-file "~/.emacs.d/themes/ramangalahy.el")
 
-(load-theme 'calmer-forest t)
+(setq prelude-theme nil)
+(load-file "~/.emacs.d/themes/autumn-light.el")
+;; (require 'autumn-light)
+
+;; (prelude-require-packages '(calmer-forest-theme))
+;; (load-theme 'calmer-forest t)
+;; (load-theme 'ramangalahy t)
+
+
+(prelude-require-packages '(grep-a-lot))
 (require 'grep-a-lot)
 
 (provide 'yf-theme)
